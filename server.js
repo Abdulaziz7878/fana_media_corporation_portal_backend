a3648f5795cebdbedd66c57e4b24a00694e7baed
meaza kebebew <meazakebebew1995@gmail.com>
2025-07-27 11:31:47 -0700

    user and video management

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
 const app = express();
+const userRoutes = require('./routes/userRoutes');
+const videoRoutes = require('./routes/videoRoutes');
+const path = require('path');
 
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Serve video files
+
 app.use('/api/auth', authRoutes);
-app.use('/uploads', express.static('uploads')); // Serve video files
+app.use('/api/users', userRoutes);
+app.use('/api/videos', videoRoutes);
 
 app.get('/', (req, res) => {
     res.send('Fana Video Portal Backend Running');
